ebbfe22eabb53b88fafbf9f5da49851edcd0bc42
Vinicius Soares <viniciussoaresmbr@gmail.com>
2022-04-18 23:35:35 -0300

    criando página da Retrospectiva

---
+++ src/pages/RetrospectiveBoard/index.tsx
@@ -0,0 +1,301 @@
+import React, { useEffect, useState } from 'react';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
+import { useLocation } from 'react-router-dom';
+import {
+  ColumnsContainer,
+  ColumnsLabel,
+  DragDrogColumns,
+  DragDrogContainer,
+  DroppableArea,
+  EvaluateArea,
+  EvaluateRetroButton,
+  EvaluateRetroText,
+  NoteCard,
+  NoteText,
+  RadioButton,
+  RadioButtonContainer,
+  RadioButtonLabel,
+  RadioButtonWrapper,
+  RetroButton,
+  RetroDescription,
+  RetroForm,
+  RetroHeader,
+  RetroInput,
+  RetroLabel,
+  RetroSection,
+  RetroTitle,
+  RetroVotes,
+} from './styles';
+import { v4 as idGenerator } from 'uuid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Note {
+  id?: string;
+  text: string;
+  status: string;
+}
+
+const RetrospectiveBoard = () => {
+  const params = useLocation();
+  const { title, description, votes } = params.state as ParamsState;
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesLocalStorage = localStorage.getItem(`notes-${title}`);
+    return notesLocalStorage ? JSON.parse(notesLocalStorage) : [];
+  });
+
+  const columnsSetup = {
+    ['notes']: {
+      name: 'Todas as anotações',
+      items: notes,
+    },
+    ['positivePoints']: {
+      name: 'Pontos Positivos',
+      items: [],
+    },
+    ['negativePoints']: {
+      name: 'Pontos Negativos',
+      items: [],
+    },
+    ['inProgress']: {
+      name: 'Em Progresso',
+      items: [],
+    },
+  };
+
+  useEffect(() => {
+    addNotesToLocalStorage();
+    setColumns(columnsSetup);
+  }, [notes]);
+
+  const addNotesToLocalStorage = () => {
+    localStorage.setItem(`notes-${title}`, JSON.stringify(notes));
+  };
+
+  const [columns, setColumns] = useState<IColumns>(columnsSetup);
+  const [radioSelect, setRadioSelect] = useState<string>('positive');
+  const [noteText, setNoteText] = useState<string>('');
+  const [allowDraggable, setAllowDraggable] = useState(false);
+
+  interface IColumns {
+    [columns: string]: {
+      name: string;
+      items: {
+        id?: string;
+        text: string;
+        status: string;
+      }[];
+    };
+  }
+
+  const onDragEnd = (
+    result: DropResult,
+    columns: IColumns,
+    setColumns: React.Dispatch<React.SetStateAction<IColumns>>,
+  ) => {
+    if (!result.destination || !allowDraggable) {
+      toast.error('Não é possível arrastar !');
+      return;
+    }
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
+  };
+
+  interface ParamsState {
+    title: string;
+    description: string;
+    votes: number;
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNoteText(e.target.value);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioSelect(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!noteText) {
+      toast.error('Nota não pode estar vazia');
+      return;
+    }
+
+    setNotes([
+      ...notes,
+      { id: idGenerator(), text: noteText, status: radioSelect },
+    ]);
+  };
+
+  const handleEvaluateButton = () => {
+    setAllowDraggable(!allowDraggable);
+    if (allowDraggable) setColumns(columnsSetup);
+  };
+
+  const EvaluateTextsValues = {
+    startEvaluation:
+      'Obs: Para liberar o modo de avaliação , onde é possível arrastar as notas é preciso iniciar a avaliação.',
+    finalizeEvaluation:
+      'Obs: Não é possível adicionar notas no modo de avaliação , finalize a avaliação e adicione mais notas',
+  };
+
+  return (
+    <RetroSection>
+      <RetroHeader>
+        <RetroLabel>Nome:</RetroLabel>
+        <RetroTitle>{title}</RetroTitle>
+        <RetroLabel>Descrição:</RetroLabel>
+        <RetroDescription>{description}</RetroDescription>
+        <RetroLabel>Votos:</RetroLabel>
+        <RetroVotes>{votes}</RetroVotes>
+      </RetroHeader>
+      {!allowDraggable && (
+        <RetroForm onSubmit={e => handleSubmit(e)}>
+          <RetroInput
+            value={noteText}
+            type="text"
+            name="note"
+            placeholder="O que rolou na Sprint ?"
+            onChange={e => handleInputChange(e)}
+          />
+          <RetroButton type="submit">Criar Nota</RetroButton>
+          <RadioButtonContainer>
+            <RadioButtonWrapper>
+              <RadioButton
+                type="radio"
+                name="radio"
+                value="positive"
+                checked={radioSelect === 'positive'}
+                onChange={e => handleSelectChange(e)}
+              />
+              <RadioButtonLabel />
+            </RadioButtonWrapper>
+            <RetroLabel fontSize="1rem" margin="1rem">
+              Ponto Positivo
+            </RetroLabel>
+            <RadioButtonWrapper>
+              <RadioButton
+                type="radio"
+                name="radio"
+                value="negative"
+                checked={radioSelect === 'negative'}
+                onChange={e => handleSelectChange(e)}
+              />
+              <RadioButtonLabel />
+            </RadioButtonWrapper>
+            <RetroLabel fontSize="1rem" margin="1rem">
+              Ponto Negativo
+            </RetroLabel>
+          </RadioButtonContainer>
+        </RetroForm>
+      )}
+      <EvaluateArea>
+        <EvaluateRetroText>
+          {allowDraggable
+            ? EvaluateTextsValues.finalizeEvaluation
+            : EvaluateTextsValues.startEvaluation}
+        </EvaluateRetroText>
+
+        <EvaluateRetroButton
+          allowDraggable={allowDraggable}
+          onClick={() => handleEvaluateButton()}
+        >
+          {!allowDraggable ? 'Iniciar avaliação' : 'Finalizar avaliação'}
+        </EvaluateRetroButton>
+      </EvaluateArea>
+      <DragDrogContainer>
+        <DragDropContext
+          onDragEnd={result => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column]) => {
+            return (
+              <DragDrogColumns key={columnId}>
+                <ColumnsLabel>{column.name}</ColumnsLabel>
+                <ColumnsContainer>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <DroppableArea
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          isDragging={snapshot.isDraggingOver}
+                          allowDraggable={!allowDraggable}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id as string}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <NoteCard
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      isDragging={snapshot.isDragging}
+                                      radioSelect={item.status}
+                                    >
+                                      <NoteText>{item.text}</NoteText>
+                                    </NoteCard>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </DroppableArea>
+                      );
+                    }}
+                  </Droppable>
+                </ColumnsContainer>
+              </DragDrogColumns>
+            );
+          })}
+        </DragDropContext>
+      </DragDrogContainer>
+      <ToastContainer autoClose={2000} />
+    </RetroSection>
+  );
+};
+
+export default RetrospectiveBoard;
---
+++ src/pages/RetrospectiveBoard/styles.ts
@@ -0,0 +1,256 @@
+import styled from 'styled-components';
+
+export const RetroSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export const RetroHeader = styled.header`
+  width: 100%;
+  min-height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.gray};
+`;
+
+interface RetroProps {
+  fontSize?: string;
+  margin?: string;
+}
+
+export const RetroLabel = styled.label<RetroProps>`
+  font-family: ${({ theme }) => theme.fonts.default};
+  font-size:  ${({ fontSize }) => (fontSize ? fontSize : '1.3rem')}
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  margin-left: ${({ margin }) => (margin ? margin : '1.5rem')};
+  margin-right: 1rem;
+`;
+
+export const RetroTitle = styled.h1`
+  font: 1.1rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  word-break: break-word;
+`;
+
+export const RetroDescription = styled.p`
+  font: 1.1rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  word-break: break-word;
+`;
+
+export const RetroVotes = styled.p`
+  font: 1.1rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+`;
+
+export const RetroForm = styled.form`
+  width: 80%;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.light};
+  margin-top: 1rem;
+  border-radius: 0.3rem;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+`;
+
+export const RetroInput = styled.input`
+  width: 40%;
+  height: 2rem;
+  margin: 0 0.5rem;
+  padding: 0 0.5rem;
+  font: 1.1rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  background-color: ${({ theme }) => theme.colors.light};
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 0.3rem;
+  outline: none;
+  transition: ease 0.3s;
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.colors.primaryLight};
+  }
+`;
+
+export const RadioButtonContainer = styled.section`
+  width: 40%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const RadioButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 48px;
+  position: relative;
+`;
+
+export const RadioButtonLabel = styled.label`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background: white;
+  border: 1px solid #ced4da};
+  margin: 0 0.5rem;
+`;
+
+export const RadioButton = styled.input`
+  cursor: pointer;
+  opacity: 0;
+  z-index: 1;
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  &:hover ~ ${RadioButtonLabel} {
+    background: #ced4da;
+    &::after {
+      content: '';
+      display: block;
+      border-radius: 50%;
+      width: 12px;
+      height: 12px;
+      margin: 5px;
+      background: #eeeeee;
+    }
+  }
+  ${props =>
+    props.checked &&
+    ` 
+    &:checked + ${RadioButtonLabel} {
+      background: #51C4D3;
+      border: 1px solid #51C4D3;
+      &::after {
+        content: "";
+        display: block;
+        border-radius: 50%;
+        width: 12px;
+        height: 12px;
+        margin: 5px;
+        box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.1);
+        background: white;
+      }
+    }
+  `}
+`;
+
+export const RetroButton = styled.button`
+  font: 1rem ${({ theme }) => theme.fonts.default};
+  width: 8rem;
+  height: 2rem;
+  border-radius: 0.3rem;
+  color: ${({ theme }) => theme.colors.light};
+  background-color: ${({ theme }) => theme.colors.primaryLight};
+  cursor: pointer;
+  transition: ease 0.3s;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
+`;
+
+export const DragDrogContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export const DragDrogColumns = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const ColumnsLabel = styled.label`
+  font: 1.3rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  margin: 1rem 0;
+`;
+
+export const ColumnsContainer = styled.div`
+  margin: 0.5rem;
+  border-radius: 0.5rem;
+`;
+
+interface DragDrogProps {
+  isDragging: boolean;
+  allowDraggable?: boolean;
+  radioSelect?: string;
+}
+
+export const DroppableArea = styled.div<DragDrogProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20rem;
+  min-height: 100vh;
+  padding: 0.3rem;
+  border-radius: 0.3rem;
+  transition: ease 0.2s;
+  background-color: ${({ isDragging, allowDraggable }) => {
+    if (allowDraggable) {
+      return allowDraggable && isDragging ? '#FBC5C6' : '#D8E3E7 ';
+    }
+    return isDragging ? '#C0D2D8' : '#D8E3E7 ';
+  }};
+`;
+
+export const NoteCard = styled.div<DragDrogProps>`
+  width: 19rem;
+  height: 5rem;
+  border-radius: 0.2rem;
+  margin: 0.3rem 0;
+  padding: 0.5rem;
+  background-color: ${({ isDragging, radioSelect }) => {
+    if (radioSelect === 'negative') {
+      return radioSelect && isDragging ? '#B5171A' : '#E32629';
+    }
+    return isDragging ? '#0C4C5A' : '#126E82';
+  }};
+  user-select: none;
+`;
+
+export const NoteText = styled.p`
+  font: 1.2rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  color: ${({ theme }) => theme.colors.light};
+`;
+
+export const EvaluateArea = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 3rem;
+  background-color: #c0d2d8;
+  margin: 1rem 0;
+`;
+
+interface EvaluateButtonProps {
+  allowDraggable?: boolean;
+}
+
+export const EvaluateRetroButton = styled.button<EvaluateButtonProps>`
+  padding: 0 0.3rem;
+  height: 2rem;
+  margin: 1rem 2rem;
+  font: 1.3rem ${({ theme }) => theme.fonts.title};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  background-color: ${({ theme, allowDraggable }) =>
+    allowDraggable ? '#ba181b' : theme.colors.primary};
+  color: ${({ theme }) => theme.colors.light};
+  border-radius: 0.2rem;
+  cursor: pointer;
+`;
+
+export const EvaluateRetroText = styled.p`
+  font: 1.2rem ${({ theme }) => theme.fonts.default};
+  font-weight: ${({ theme }) => theme.fonts.weight.regular};
+  margin: 1rem 0;
+`;
