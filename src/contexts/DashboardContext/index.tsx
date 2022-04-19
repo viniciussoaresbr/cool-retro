01ec3368237ae6a946e230710b6b614423a7397d
Vinicius Soares <viniciussoaresmbr@gmail.com>
2022-04-18 23:29:19 -0300

    criando DashboardContext

--- src/contexts/DashboardContext/index.tsx
+++ src/contexts/DashboardContext/index.tsx
@@ -9,20 +9,24 @@ interface Dashboard {
   votes: string;
 }
 
-interface ContextInterface {
+interface DashBoardContextInterface {
   createDashboards: (data: Dashboard) => void;
   updateDashboards: (id: string, data: Dashboard) => void;
   deleteDashboards: (id: string) => void;
   dashboards: Dashboard[];
   navigateToPage: (dashboard: Dashboard) => void;
+  addDashboardsToLocalStorage: () => void;
 }
 
-export const DashboardContext = createContext<ContextInterface>(
-  {} as ContextInterface,
+export const DashboardContext = createContext<DashBoardContextInterface>(
+  {} as DashBoardContextInterface,
 );
 
 const DashboardProvider: React.FC = ({ children }) => {
-  const [dashboards, setDashboards] = useState<Dashboard[]>([]);
+  const [dashboards, setDashboards] = useState<Dashboard[]>(() => {
+    const dashboardsLocalStorage = localStorage.getItem('dashboards');
+    return dashboardsLocalStorage ? JSON.parse(dashboardsLocalStorage) : [];
+  });
   const navigate = useNavigate();
 
   const createDashboards = (data: Dashboard) => {
@@ -58,6 +62,10 @@ const DashboardProvider: React.FC = ({ children }) => {
     });
   };
 
+  const addDashboardsToLocalStorage = () => {
+    localStorage.setItem('dashboards', JSON.stringify(dashboards));
+  };
+
   return (
     <DashboardContext.Provider
       value={{
@@ -66,6 +74,7 @@ const DashboardProvider: React.FC = ({ children }) => {
         deleteDashboards,
         dashboards,
         navigateToPage,
+        addDashboardsToLocalStorage,
       }}
     >
       {children}
